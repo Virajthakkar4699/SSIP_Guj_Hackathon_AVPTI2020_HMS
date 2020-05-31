422c6b108e81997e3da29e5fb744dc7ee7e6969e
Viraj Thakkar <viraj.sachde@gmail.com>
2020-05-31 11:44:04 +0530

    Android App
    QR Code

---
+++ Android/AVPTIHOSTEL/app/src/main/java/com/example/avptihostel/newactivity.kt
@@ -0,0 +1,14 @@
+
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import app.com.sample.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_another)
+    }
+}
---
+++ Android/avpti/app/src/main/java/com/example/avpti/s3.kt
@@ -0,0 +1,12 @@
+package com.example.avpti
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class s3 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_s3)
+    }
+}
